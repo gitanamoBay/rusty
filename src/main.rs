a1c62df860852f3407f45e6211a63d7c96297507
Hashtaggins <gidgetbreath@hotmail.com>
2015-07-21 23:16:51 +0100

    as it is

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,14 @@ Usage:
 
 Options:
     -h --help  Show this help message
+
+
+some commands
+    add     Add a new note, optional args for status
+    list    Lists all notes
+    update  Update a note with id to a new status
+    remove  Remve note with id
+
 ");
 
 //    rusty add <thing>
@@ -28,7 +36,13 @@ Options:
 //    rusty update <id> <newStatus>
 //    rusty remove <id>
 //    rusty --help
-
+#[derive(Debug)]
+struct Args {
+    arg_command: Option<Command>,
+    arg_args: Vec<String>,
+    flag_list: bool,
+    flag_verbose: bool,
+}
 
 #[derive(Debug)]
 enum Command{
@@ -41,14 +55,15 @@ pub struct Collection{
 fn main() {
     let args: Args = Args::docopt().decode().unwrap_or_else(|e| e.exit());
       
-    if(args.cmd_add)
-    {
-        println!("{}",args.arg_thing);
-    }
-    
-    if(args.cmd_remove)
-    {
-    }
+  //  if(args.cmd_add)
+  //  {
+  //      println!("{}",args.arg_thing);
+  //  }
+  //  
+  //  if(args.cmd_remove)
+  //  {
+  //  }
+
 
     
 //    let ent = Entry{
