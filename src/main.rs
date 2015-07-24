f7e463ccebe8db98929be5b279c487e5136269db
Hashtaggins <gidgetbreath@hotmail.com>
2015-07-24 01:02:38 +0100

    didn't get quite as far with it as planned, but it is storing a collection atleast

--- src/main.rs
+++ src/main.rs
@@ -33,7 +33,8 @@ Options:
 fn main() {
     let args: Args = Args::docopt().decode().unwrap_or_else(|e| e.exit());
     
-    let filepath = Path::new("foo.txt");
+    println!("{:#?}", args);
+    let filepath = Path::new("itsbroke.txt");
     let mut file = if filepath.exists() {
         match OpenOptions::new().read(true).write(true).open(filepath) {
             Ok(file) => file,
@@ -48,19 +49,29 @@ fn main() {
     
     if args.cmd_add {
         //we love egyptian braces.
+        //let status = if (args.arg_status.is_empty()) {
+        //    "new" 
+        //} else {
+        //    args.arg_status
+        //};
+
+
+        let ent = Entry::new(1,"1","1");
+        let ent1 = Entry::new(2,"2","2");
+        let ent2 = Entry::new(3,"3","3");
+
         
+        //let mut v = vec![json::encode(&ent).unwrap(),json::encode(&ent1).unwrap(),json::encode(&ent2).unwrap()];
+        let mut v = vec![ent,ent1,ent2];
+        
+        let encoded = json::encode(&v).unwrap();
+         
+        match file.write_all(encoded.as_bytes()) {
+            Ok(_) => println!("worked."),
+            Err(why) => panic!("errored on write:  {}",why),
+        }
     }
 
-    let ent = Entry::new(1,"test","test");
-
-    let encoded = json::encode(&ent).unwrap();
-    
-    match file.write_all(encoded.as_bytes()) {
-        Ok(_) => println!("worked."),
-        Err(why) => panic!("errored on write:  {}",why),
-    }
-
-   // println!("{:#?}", args);
 
     //if(args.cmd_add)
     //{
